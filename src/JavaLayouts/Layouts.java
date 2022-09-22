f9cfb4d9c7a5634c2cea111f28ba801d9e7728f6
Ailton <ailtonc.n64@gmail.com>
2022-09-22 07:21:46 -0300

    inciando o layout

--- src/JavaLayouts/Layouts.java
+++ src/JavaLayouts/Layouts.java
@@ -1,8 +1,23 @@
 package JavaLayouts;
 
-// import java.util.Scanner;
+ import java.util.Scanner;
 
 // classe para gráficos
 public class Layouts {
+
+    public void mainTitle() {
+        Scanner in = new Scanner(System.in);
+
+        int optionChoosed = 0;
+
+        // layout da páina com esse caractere
+    System.out.println("•");
+
+    // entrada de dados e limpeza do buffer
+    optionChoosed = in.nextInt(); in.nextLine();
+
+    in.close();
+
+    }
 
 }
